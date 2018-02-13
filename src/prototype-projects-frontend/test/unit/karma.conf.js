fa94d1d0a3fde9f8b5bb518dd87921af45f748ab
Graham Bartley <grabartley@gmail.com>
2018-02-13 22:03:58 +0000

    modify ci configuration

--- src/prototype-projects-frontend/test/unit/karma.conf.js
+++ src/prototype-projects-frontend/test/unit/karma.conf.js
@@ -6,16 +6,8 @@
 var webpackConfig = require('../../build/webpack.test.conf')
 
 module.exports = function karmaConfig (config) {
-  let browser = 'HeadlessChrome';
-  if (process.env.GITLAB_CI) {
-    browser = 'PhantomJS';
-  }
   config.set({
-    // to run in additional browsers:
-    // 1. install corresponding karma launcher
-    //    http://karma-runner.github.io/0.13/config/browsers.html
-    // 2. add it to the `browsers` array below.
-    browsers: [browser],
+    browsers: ['HeadlessChrome'],
     customLaunchers: {
       HeadlessChrome: {
         base: 'Chrome',
